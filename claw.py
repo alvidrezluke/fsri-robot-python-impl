0f9bd849d74dbcc6c3aa0d726e19fac9153bd533
Luke Alvidrez <alvidrezluke@gmail.com>
2022-08-24 02:50:43 -0700

    plz

--- claw.py
+++ claw.py
@@ -15,18 +15,51 @@ def open_claw():
     state = get_claw_state();
     match state:
         case "unknown":
+            print("Do not know state of claw")
             move_claw_to_open()
         case "open":
             print("Claw already open")
         case "closed":
             print("Opening claw")
-            # TODO: Actually open the claw
+            move_claw_to_open()
         case "opening":
             print("Already opening")
         case "closing":
             print("Already closing")
         case _:
             print("Invalid state")
+import Jetson.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
 
 def move_claw_to_open():
-    print("Open plz")
+    try:
+        while True:
+            p.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(12.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(2.5)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        p.stop()
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    open_claw()
